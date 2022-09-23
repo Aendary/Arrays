66ebfb5706aa96d981006ad5fed213b48da099f6
Aendary Elia <aendary.elia@mrjaffesclass.com>
2022-09-23 11:42:58 -0700

    array

--- TestClass.java
+++ TestClass.java
@@ -1,15 +1,19 @@
-
-/**
- * Write a description of class RenameThisClass here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class TestClass
 {
     public static void main(String[] args) {
         int i;
         float f = 2.5f;
         boolean flag = true;
+        String[] array = {"Alpha","Bravo","Charlie","Delta","Echo"};
+        for (int Starray = 0; Starray < array.length; Starray++) {
+            System.out.println(array[Starray]);
+            
+        }
+        for (String value : array) {
+            System.out.println(value);
+        }
+        System.out.println();
+        for (int Starray = 0; Starray < array.length; Starray++)
+            System.out.println("-"); //Alpha-Bravo-Charlie-Delta-Echo
     }
 }
